e7087334bba2ce1342bcd22de3d2ef4a8f65622a
Dejan Gregor <5954680+api-py@users.noreply.github.com>
2025-09-06 18:41:59 +0100

    Update dcert.rb

--- Formula/dcert.rb
+++ Formula/dcert.rb
@@ -1,8 +1,8 @@
 class Dcert < Formula
   desc "CLI to decode and validate TLS certificates from PEM files"
   homepage "https://github.com/SCGIS-Wales/dcert"
-  url "https://github.com/SCGIS-Wales/dcert/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "67b6a577ed70174dbab50f9c2d9df58a7714d4610488d0bdddeee441b7961efb"
+  url "https://github.com/SCGIS-Wales/dcert/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "46c6932da29418ace2408c7af388cbe5c9d991808661385998440ada4c72f3a2"
   license "MIT"
   head "https://github.com/SCGIS-Wales/dcert.git", branch: "main"
 
@@ -13,7 +13,6 @@ class Dcert < Formula
   end
 
   test do
-    # Version check is architecture independent and avoids needing a real PEM
     assert_match version.to_s, shell_output("#{bin}/dcert --version")
   end
 end
