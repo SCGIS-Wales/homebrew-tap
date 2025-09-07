49b867033eb35cdc350c345ce6c5d724f33b275e
Dejan Gregor <5954680+api-py@users.noreply.github.com>
2025-09-07 07:57:28 +0100

    Update dcert.rb

--- Formula/dcert.rb
+++ Formula/dcert.rb
@@ -2,7 +2,7 @@ class Dcert < Formula
   desc "CLI to decode and validate TLS certificates from PEM files"
   homepage "https://github.com/SCGIS-Wales/dcert"
   url "https://github.com/SCGIS-Wales/dcert/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "40cc977e2b3900eb155484c66e54f069a2c682f6ea2bbddfb15fd9397577cf25"
+  sha256 "749beb3509ccff0f84bc94acc6ed4ad15ada2bb54f618a1573c9aada1f59f48b"
   license "MIT"
   head "https://github.com/SCGIS-Wales/dcert.git", branch: "main"
 
