8c465b759ac5e79f6772de77586203bcdf34f099
Dejan Gregor <5954680+api-py@users.noreply.github.com>
2025-09-14 15:52:43 +0100

    Update SHA256 checksums for dcert binaries

--- Formula/dcert.rb
+++ Formula/dcert.rb
@@ -7,7 +7,7 @@ class Dcert < Formula
   on_macos do
     on_intel do
       url "https://github.com/SCGIS-Wales/dcert/releases/download/v1.0.0/dcert-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_MACOS_X86_SHA256"
+      sha256 "f4740e4855f01ed28ad2a7c4751d3d4d9304f09c35d8007d1414ed6f14889f06"
 
       def install
         bin.install "dcert"
@@ -16,7 +16,7 @@ class Dcert < Formula
 
     on_arm do
       url "https://github.com/SCGIS-Wales/dcert/releases/download/v1.0.0/dcert-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_MACOS_ARM_SHA256"
+      sha256 "505135e9c33ffd9df7dd4780d1346d96d6a541a2b32350131aaf5514696a15cf"
 
       def install
         bin.install "dcert"
@@ -27,7 +27,7 @@ class Dcert < Formula
   on_linux do
     on_intel do
       url "https://github.com/SCGIS-Wales/dcert/releases/download/v1.0.0/dcert-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_LINUX_X86_SHA256"
+      sha256 "30261b7190a502c403a73e1d0b69bc66f5ede418381cce3108aeff0899b2eb3f"
 
       def install
         bin.install "dcert"
